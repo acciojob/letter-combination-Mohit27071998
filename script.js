00f168bc6c1ee4a6337fe79a5fe7733499cb70e8
Mohit Kamdi <130160983+Mohit27071998@users.noreply.github.com>
2023-05-15 17:13:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
-function letterCombinations(input_digit) {
+let mapping = {
+	"2":"abc",
+	"3":"def",
+	"4":"ghi",
+	"5":"jkl",
+	"6":"mno",
+	"7":"pqrs",
+	"8":"tuv",
+	"9":"wxyz"
+}
+let res = [];
+let sstr = "23";
+
+
+function letterCombinations(input_digit,ans) {
   //Complete the function
+	if(input_digit > sstr.length){
+		res.push(ans);
+		return;
+	}
+	let currstr = mapping[sstr[input_digit]]
+	for(let i = 0; i < currstr.length;i++){
+		solve(input_digit + 1, ans + currstr[i]);
+	}
 }
 
 module.exports = letterCombinations;
