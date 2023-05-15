e681b561aa5ec28dafa64c3be32c103a20ea268b
Mohit Kamdi <130160983+Mohit27071998@users.noreply.github.com>
2023-05-15 17:36:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,20 +8,22 @@ let mapping = {
 	"8":"tuv",
 	"9":"wxyz"
 }
+
+
 let res = [];
-let sstr = "23";
-
-
 function letterCombinations(input_digit,ans) {
   //Complete the function
-	if(input_digit > sstr.length){
+  
+	if(input_digit >= sstr.length){
 		res.push(ans);
+		// console.log(res)
 		return;
 	}
 	let currstr = mapping[sstr[input_digit]]
+// 	console.log(currstr)
 	for(let i = 0; i < currstr.length;i++){
-		solve(input_digit + 1, ans + currstr[i]);
+		letterCombinations(input_digit + 1, ans + currstr[i]);
 	}
+	return res;
 }
-
 module.exports = letterCombinations;
