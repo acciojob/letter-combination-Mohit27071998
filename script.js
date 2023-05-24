e827998a1934bb2d39023a45643fbc5844a9bea1
Mohit Kamdi <130160983+Mohit27071998@users.noreply.github.com>
2023-05-24 11:29:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,31 +1,36 @@
+let mapping = {
+	"0" : "0",
+	"1" : "1",
+	"2":"abc",
+	"3":"def",
+	"4":"ghi",
+	"5":"jkl",
+	"6":"mno",
+	"7":"pqrs",
+	"8":"tuv",
+	"9":"wxyz"
+}
 
 
-function letterCombinations(digits) {
-  const digitToLetters = {
-    '2': 'abc',
-    '3': 'def',
-    '4': 'ghi',
-    '5': 'jkl',
-    '6': 'mno',
-    '7': 'pqrs',
-    '8': 'tuv',
-    '9': 'wxyz'
-  };
-
-  const result = [];
-
-  function backtrack(combination, nextDigits) {
-    if (nextDigits.length === 0) {
-      result.push(combination);
-    } else {
-      const currentDigit = nextDigits[0];
-      const letters = digitToLetters[currentDigit];
-
-      for (let i = 0; i < letters.length; i++) {
-        const letter = letters[i];
-        backtrack(combination + letter, nextDigits.slice(1));
-      }
+let res = [];
+function helper(idx,ans,sstr){
+  // 	console.log(idx)
+  	if(idx >= sstr.length){
+		res.push(ans);
+		// console.log(res)
+		return;
 	}
-  }
+	let currstr = mapping[sstr[idx]]
+// 	console.log(currstr)
+	for(let i = 0; i < currstr.length;i++){
+		helper(idx + 1, ans + currstr[i],sstr);
+	}
+	return res.sort();
+}
+function letterCombinations(input_digit) {
+  //Complete the function
+  return helper(0,"",input_digit);
+  
+
 }
 module.exports = letterCombinations;
